5f8096c240a6c107ef1c8a8b690f0def4083dace
ManikumarJ <manikumarj6@gmail.com>
2025-05-26 14:42:20 +0530

    26-05-25

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,189 +1,12 @@
-// import {
-//   Instagram,
-//   Linkedin,
-//   Mail,
-//   MapPin,
-//   Phone,
-//   Send,
-//   Twitch,
-//   Twitter,
-// } from "lucide-react";
-// import { cn } from "@/lib/utils";
-// import { useToast } from "@/hooks/use-toast";
-// import { useState } from "react";
-
-// export const ContactSection = () => {
-//   const { toast } = useToast();
-//   const [isSubmitting, setIsSubmitting] = useState(false);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     setIsSubmitting(true);
-
-//     setTimeout(() => {
-//       toast({
-//         title: "Message sent!",
-//         description: "Thank you for your message. I'll get back to you soon.",
-//       });
-//       setIsSubmitting(false);
-//     }, 1500);
-//   };
-//   return (
-//     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
-//       <div className="container mx-auto max-w-5xl">
-//         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-//           Get In <span className="text-primary"> Touch</span>
-//         </h2>
-
-//         <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-//           Have a project in mind or want to collaborate? Feel free to reach out.
-//           I'm always open to discussing new opportunities.
-//         </p>
-
-//         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-//           <div className="space-y-8">
-//             <h3 className="text-2xl font-semibold mb-6">
-//               {" "}
-//               Contact Information
-//             </h3>
-
-//             <div className="space-y-6 justify-center">
-//               <div className="flex items-start space-x-4">
-//                 <div className="p-3 rounded-full bg-primary/10">
-//                   <Mail className="h-6 w-6 text-primary" />{" "}
-//                 </div>
-//                 <div>
-//                   <h4 className="font-medium"> Email</h4>
-//                   <a
-//                     href="mailto:manikumarj@gmail.com"
-//                     className="text-muted-foreground hover:text-primary transition-colors"
-//                   >
-//                     manikumarj6@gmail.com
-//                   </a>
-//                 </div>
-//               </div>
-//               <div className="flex items-start space-x-4">
-//                 <div className="p-3 rounded-full bg-primary/10">
-//                   <Phone className="h-6 w-6 text-primary" />{" "}
-//                 </div>
-//                 <div>
-//                   <h4 className="font-medium"> Phone</h4>
-//                   <a
-//                     href="tel:+918248915910"
-//                     className="text-muted-foreground hover:text-primary transition-colors"
-//                   >
-//                     +91 82489 15910
-//                   </a>
-//                 </div>
-//               </div>
-//               <div className="flex items-start space-x-4">
-//                 <div className="p-3 rounded-full bg-primary/10">
-//                   <MapPin className="h-6 w-6 text-primary" />{" "}
-//                 </div>
-//                 <div>
-//                   <h4 className="font-medium"> Location</h4>
-//                   <a className="text-muted-foreground hover:text-primary transition-colors">
-//                     Theni 625 531, Tamil Nadu, India
-//                   </a>
-//                 </div>
-//               </div>
-//             </div>
-
-//             <div className="pt-8">
-//               <h4 className="font-medium mb-4"> Connect With Me</h4>
-//               <div className="flex space-x-4 justify-center">
-//                 <a href="www.linkedin.com/in/manikumarj" target="_blank">
-//                   <Linkedin />
-//                 </a>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div
-//             className="bg-card p-8 rounded-lg shadow-xs"
-//             onSubmit={handleSubmit}
-//           >
-//             <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
-
-//             <form className="space-y-6">
-//               <div>
-//                 <label
-//                   htmlFor="name"
-//                   className="block text-sm font-medium mb-2"
-//                 >
-//                   {" "}
-//                   Your Name
-//                 </label>
-//                 <input
-//                   type="text"
-//                   id="name"
-//                   name="name"
-//                   required
-//                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-//                   placeholder="Manikumar J..."
-//                 />
-//               </div>
-
-//               <div>
-//                 <label
-//                   htmlFor="email"
-//                   className="block text-sm font-medium mb-2"
-//                 >
-//                   {" "}
-//                   Your Email
-//                 </label>
-//                 <input
-//                   type="email"
-//                   id="email"
-//                   name="email"
-//                   required
-//                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-//                   placeholder="manikumarj6@gmail.com"
-//                 />
-//               </div>
-
-//               <div>
-//                 <label
-//                   htmlFor="message"
-//                   className="block text-sm font-medium mb-2"
-//                 >
-//                   {" "}
-//                   Your Message
-//                 </label>
-//                 <textarea
-//                   id="message"
-//                   name="message"
-//                   required
-//                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
-//                   placeholder="Hello, I'd like to talk about..."
-//                 />
-//               </div>
-
-//               <button
-//                 type="submit"
-//                 disabled={isSubmitting}
-//                 className={cn(
-//                   "cosmic-button w-full flex items-center justify-center gap-2"
-//                 )}
-//               >
-//                 {isSubmitting ? "Sending..." : "Send Message"}
-//                 <Send size={16} />
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
 import {
-  Mail,
-  Phone,
-  MapPin,
+  Instagram,
   Linkedin,
+  Mail,
+  MapPin,
+  Phone,
   Send,
+  Twitch,
+  Twitter,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
@@ -195,6 +18,7 @@ export const ContactSection = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     setIsSubmitting(true);
 
     setTimeout(() => {
@@ -205,12 +29,11 @@ export const ContactSection = () => {
       setIsSubmitting(false);
     }, 1500);
   };
-
   return (
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-          Get In <span className="text-primary">Touch</span>
+          Get In <span className="text-primary"> Touch</span>
         </h2>
 
         <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
@@ -219,32 +42,33 @@ export const ContactSection = () => {
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          {/* Contact Info */}
           <div className="space-y-8">
-            <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+            <h3 className="text-2xl font-semibold mb-6">
+              {" "}
+              Contact Information
+            </h3>
 
             <div className="space-y-6 justify-center">
               <div className="flex items-start space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <Mail className="h-6 w-6 text-primary" />
+                  <Mail className="h-6 w-6 text-primary" />{" "}
                 </div>
                 <div>
-                  <h4 className="font-medium">Email</h4>
+                  <h4 className="font-medium"> Email</h4>
                   <a
-                    href="mailto:manikumarj6@gmail.com"
+                    href="mailto:manikumarj@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
                     manikumarj6@gmail.com
                   </a>
                 </div>
               </div>
-
               <div className="flex items-start space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <Phone className="h-6 w-6 text-primary" />
+                  <Phone className="h-6 w-6 text-primary" />{" "}
                 </div>
                 <div>
-                  <h4 className="font-medium">Phone</h4>
+                  <h4 className="font-medium"> Phone</h4>
                   <a
                     href="tel:+918248915910"
                     className="text-muted-foreground hover:text-primary transition-colors"
@@ -253,42 +77,42 @@ export const ContactSection = () => {
                   </a>
                 </div>
               </div>
-
               <div className="flex items-start space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <MapPin className="h-6 w-6 text-primary" />
+                  <MapPin className="h-6 w-6 text-primary" />{" "}
                 </div>
                 <div>
-                  <h4 className="font-medium">Location</h4>
-                  <span className="text-muted-foreground hover:text-primary transition-colors">
+                  <h4 className="font-medium"> Location</h4>
+                  <a className="text-muted-foreground hover:text-primary transition-colors">
                     Theni 625 531, Tamil Nadu, India
-                  </span>
+                  </a>
                 </div>
               </div>
             </div>
 
             <div className="pt-8">
-              <h4 className="font-medium mb-4">Connect With Me</h4>
+              <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a
-                  href="https://www.linkedin.com/in/manikumarj"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-muted-foreground hover:text-primary transition-colors"
-                >
+                <a href="www.linkedin.com/in/manikumarj" target="_blank">
                   <Linkedin />
                 </a>
               </div>
             </div>
           </div>
 
-          {/* Contact Form */}
-          <div className="bg-card p-8 rounded-lg shadow-xs">
-            <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
+          <div
+            className="bg-card p-8 rounded-lg shadow-xs"
+            onSubmit={handleSubmit}
+          >
+            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
 
-            <form className="space-y-6" onSubmit={handleSubmit}>
+            <form className="space-y-6">
               <div>
-                <label htmlFor="name" className="block text-sm font-medium mb-2">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
                   Your Name
                 </label>
                 <input
@@ -296,13 +120,17 @@ export const ContactSection = () => {
                   id="name"
                   name="name"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
                   placeholder="Manikumar J..."
                 />
               </div>
 
               <div>
-                <label htmlFor="email" className="block text-sm font-medium mb-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
                   Your Email
                 </label>
                 <input
@@ -310,20 +138,24 @@ export const ContactSection = () => {
                   id="email"
                   name="email"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
                   placeholder="manikumarj6@gmail.com"
                 />
               </div>
 
               <div>
-                <label htmlFor="message" className="block text-sm font-medium mb-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
                   Your Message
                 </label>
                 <textarea
                   id="message"
                   name="message"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary resize-none"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
                   placeholder="Hello, I'd like to talk about..."
                 />
               </div>
