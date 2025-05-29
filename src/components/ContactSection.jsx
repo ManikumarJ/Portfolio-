dd85c83b86f7e5fa7e36bcadf6f3254b8617646a
ManikumarJ <manikumarj6@gmail.com>
2025-05-29 10:11:07 +0530

    29-05-25

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -93,7 +93,8 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="www.linkedin.com/in/manikumarj" target="_blank">
+                <a href=" https://www.linkedin.com/in/manikumarj/" target="_blank">
+                
                   <Linkedin />
                 </a>
               </div>
