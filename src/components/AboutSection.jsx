47a365d5e72f5b8b7168daa7b31643771c16762d
manikumarJ <manikumarj6@gmail.com>
2025-06-13 11:10:36 +0530

    13-06-25

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -26,7 +26,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1A8mzR7me-LYHfRCVFhVfhmqi26dZub5x/view?usp=sharing"
+                href="https://drive.google.com/file/d/1nr33Qzn0MFIAtMVcdZV3sCCYCTe995od/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
